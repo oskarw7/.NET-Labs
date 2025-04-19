086fe7b6f90cdf097655ffbb2cc1a5bb8c2280a2
oskarw7 <oskarwisniewski2004@gmail.com>
2025-04-19 17:04:32 +0200

    feat(Lab1): Employee class

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Lab1
         {
             InitializeComponent();
         }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
---
+++ Lab1/Model/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Model
+{
+    public class Employee
+    {
+        private string name {  get; set; }
+        private int yearOfEmployment { get; set; }
+        private double skillLevel { get; set; }
+        private ObservableCollection<Employee> subordinates { get; set; }
+
+        public Employee(string name, int yearOfEmployment, double skillLevel, 
+            ObservableCollection<Employee> subordinates) {
+            this.name = name;
+            this.yearOfEmployment = yearOfEmployment;
+            this.skillLevel = skillLevel;
+            this.subordinates = subordinates;
+        }
+
+        public override string ToString()
+        {
+            return "Employee: name=" + name + ", year of employment=" + yearOfEmployment + ", skill level=" + skillLevel;
+        }
+    }
+}
