3dc8c0fe4102f1c106489ec5c4aad1857a7a1a82
oskarw7 <oskarwisniewski2004@gmail.com>
2025-04-22 12:56:58 +0200

    Merge dev to main

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Lab1.Model;
+using Lab1.View;
+using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +19,132 @@ namespace Lab1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Employee> employees;
         public MainWindow()
         {
             InitializeComponent();
+            employees = new ObservableCollection<Employee>();
+            EmployeeTreeView.ItemsSource = employees;
+        }
+
+        private void GenerateData_Click(object sender, RoutedEventArgs e)
+        {
+            employees.Clear();
+
+            var ceoSubordinates = new ObservableCollection<Employee>
+            {
+                new Employee("Bob", 2015, 6.5, new ObservableCollection<Employee>()),
+                new Employee("Charlie", 2018, 7.0, new ObservableCollection<Employee>()),
+                new Employee("Robert", 2018, 1.0, new ObservableCollection<Employee>()),
+                new Employee("Diana", 2020, 5.0, new ObservableCollection<Employee>()),
+                new Employee("Ewa", 2019, 4.5, new ObservableCollection<Employee>())
+            };
+
+            var ceo = new Employee("Alice", 2010, 9.0, ceoSubordinates);
+
+            ceoSubordinates[0].AddSubordinate(new Employee("Piotrek", 2022, 0.5, new ObservableCollection<Employee>()));
+            ceoSubordinates[1].AddSubordinate(new Employee("Lukasz", 2019, 4.0, new ObservableCollection<Employee>()));
+            ceoSubordinates[2].AddSubordinate(new Employee("Eustachy", 2018, 6.0, new ObservableCollection<Employee>()));
+            ceoSubordinates[3].AddSubordinate(new Employee("Grzegorz", 2018, 6.0, new ObservableCollection<Employee>()));
+            ceoSubordinates[3].AddSubordinate(new Employee("Ania", 2020, 4.5, new ObservableCollection<Employee>()));
+            ceoSubordinates[4].AddSubordinate(new Employee("Katarzyna", 2017, 5.5, new ObservableCollection<Employee>()));
+
+            var subordinatesOfFist = new ObservableCollection<Employee>
+            {
+                new Employee("Marta", 2021, 5.0, new ObservableCollection<Employee>()),
+                new Employee("Marek", 1960, 5.5, new ObservableCollection<Employee>()),
+                new Employee("Tomek", 2020, 5.0, new ObservableCollection<Employee>())
+            };
+            ceoSubordinates[0].AddSubordinate(new Employee("Franciszek", 2015, 5.5, subordinatesOfFist));
+
+            subordinatesOfFist[0].AddSubordinate(new Employee("Szymon", 2022, 3.5, new ObservableCollection<Employee>()));
+
+            var subordinatesOfTomek = new ObservableCollection<Employee>
+            {
+                new Employee("Julia", 2021, 4.5, new ObservableCollection<Employee>())
+            };
+
+            ceoSubordinates[1].AddSubordinate(new Employee("Szymon", 2021, 6.0, subordinatesOfTomek));
+
+            var subordinatesOfEwa = new ObservableCollection<Employee>
+            {
+                new Employee("Kamil", 2020, 5.0, new ObservableCollection<Employee>())
+            };
+
+            ceoSubordinates[4].AddSubordinate(new Employee("Zuzanna", 2021, 4.0, subordinatesOfEwa));
+
+            employees.Add(ceo);
+        }
+
+        private void Version_Click(object sender, RoutedEventArgs e)
+        {
+            var versionWindow = new VersionWindow();
+            versionWindow.ShowDialog();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void EmployeeTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) { 
+            var selectedEmployee = EmployeeTreeView.SelectedItem as Employee;
+            if (selectedEmployee == null) return;
+            string employeesText = selectedEmployee.PrintRecursive();
+            EmployeeDetails.Text = employeesText;
+        }
+
+        private void EmployeeTreeView_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var src = e.OriginalSource as DependencyObject;
+
+            while (src != null && src is not TreeViewItem)
+            {
+                src = VisualTreeHelper.GetParent(src);
+            }
+            var treeItem = src as TreeViewItem;
+
+            if (treeItem == null) return;
+            treeItem.Focus();
+            e.Handled = true; // przeciwdzialanie wyswietleniu defaultowemu context menu
+
+            var employee = treeItem.DataContext as Employee;
+            if (employee == null) return;
+
+            var contextMenu = new ContextMenu();
+            var createItem = new MenuItem { Header = "Create" };
+            createItem.Click += (s, ev) => CreateEmployee(employee);
+            contextMenu.Items.Add(createItem);
+            var deleteItem = new MenuItem { Header = "Delete" };
+            deleteItem.Click += (s, ev) => DeleteEmployee(employee);
+            contextMenu.Items.Add(deleteItem);
+
+            treeItem.ContextMenu = contextMenu;
+        }
+
+        public void CreateEmployee(Employee superiorEmployee)
+        {
+            var createEmployeeWindow = new CreateEmployeeWindow();
+            var result = createEmployeeWindow.ShowDialog();
+            if (result==true)
+            {
+                superiorEmployee.AddSubordinate(createEmployeeWindow.employee);
+            }
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            var supervisor = employee.supervisor;
+            if(supervisor == null)
+            {
+                employees.Remove(employee);
+            }
+            else
+            {
+                employee.supervisor.subordinates.Remove(employee);
+            }
+            EmployeeTreeView.Items.Refresh();
+            EmployeeDetails.Text = "";
         }
     }
 }
---
+++ Lab1/Model/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Model
+{
+    public class Employee
+    {
+        // Public, zeby TreeView widzialo zmienne do bindingu
+        public string name {  get; private set; }
+        public int yearOfEmployment { get; private set; }
+        public double skillLevel { get; private set; }
+        public ObservableCollection<Employee> subordinates { get; private set; }
+
+        public Employee supervisor { get; set; }
+
+        public Employee(string name, int yearOfEmployment, double skillLevel, 
+            ObservableCollection<Employee> subordinates) {
+            this.name = name;
+            this.yearOfEmployment = yearOfEmployment;
+            this.skillLevel = skillLevel;
+            this.subordinates = subordinates;
+            if(subordinates != null)
+            {
+                foreach(var subordinate in subordinates)
+                {
+                    subordinate.supervisor = this;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Employee: name=" + name + ", year of employment=" + yearOfEmployment + ", skill level=" + skillLevel;
+        }
+
+        public void AddSubordinate(Employee subordinate)
+        {
+            subordinates.Add(subordinate);
+            subordinate.supervisor = this;
+        }
+
+        public string PrintRecursive(int depth = 0)
+        {
+            string result = new string('\t', depth) + this + "\n\n";
+            foreach (var subordinate in subordinates)
+            {
+                result += subordinate.PrintRecursive(depth + 1);
+            }
+            return result;
+        }
+    }
+}
