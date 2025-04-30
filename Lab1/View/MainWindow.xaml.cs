47ac0e14ffa498d3add0766e5879d20dc5e469e3
oskarw7 <oskarwisniewski2004@gmail.com>
2025-05-01 00:30:32 +0200

    feat(Lab1): Generate data edit

--- Lab1/View/MainWindow.xaml.cs
+++ Lab1/View/MainWindow.xaml.cs
@@ -30,16 +30,7 @@ namespace Lab1.View
 
         private void GenerateData_Click(object sender, RoutedEventArgs e)
         {
-            if (employees.Any(emp => emp.name == "Alice" && emp.skillLevel == 9.0 && emp.yearOfEmployment == 2010))
-            {
-                return;
-            }
-
-            if (EmployeeTreeView.ItemsSource is ObservableCollection<Employee> newList)
-            {
-                employees = newList;
-            }
-
+            employees.Clear();
 
             var ceoSubordinates = new ObservableCollection<Employee>
             {
@@ -84,6 +75,19 @@ namespace Lab1.View
             ceoSubordinates[4].AddSubordinate(new Employee("Zuzanna", 2021, 4.0, subordinatesOfEwa));
 
             employees.Add(ceo);
+
+            var ceo2Subordinates = new ObservableCollection<Employee>
+            {
+                new Employee("Ola", 2017, 6.5, new ObservableCollection<Employee>()),
+                new Employee("Pawel", 2016, 7.5, new ObservableCollection<Employee>())
+            };
+
+            var secondRoot = new Employee("Henryk", 2008, 8.0, ceo2Subordinates);
+
+            ceo2Subordinates[0].AddSubordinate(new Employee("Basia", 2019, 5.5, new ObservableCollection<Employee>()));
+            ceo2Subordinates[1].AddSubordinate(new Employee("Janek", 2020, 6.0, new ObservableCollection<Employee>()));
+
+            employees.Add(secondRoot);
         }
 
         private void Version_Click(object sender, RoutedEventArgs e)
