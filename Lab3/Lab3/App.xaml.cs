d66cc135778a3c4a6a3e54a9de7d3871f1044516
Mikołaj Wiszniewski <mikowisz@gmail.com>
2025-05-11 16:40:00 +0200

    feat(lab3): init + zadanie 1

---
+++ Lab3/Lab3/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Lab2.View;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ Lab3/Lab3/Model/EmployeeInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Model
+{
+    public enum Status
+    {
+        Intern,
+        Junior,
+        Mid,
+        Senior
+    }
+
+    public class EmployeeInfo
+    {
+        // deserializacja wymaga publicznych setterów
+        public int yearOfEmployment { get; set; }
+        public int skillLevel { get; set; } 
+        public Status status { get; set; }
+
+        public EmployeeInfo(int yearOfEmployment, int skillLevel, Status status)
+        {
+            this.yearOfEmployment = yearOfEmployment;
+            if (skillLevel < 1) this.skillLevel = 1;
+            else if (skillLevel > 10) this.skillLevel = 10;
+            else this.skillLevel = skillLevel;
+            this.status = status;
+        }
+
+        // wymog serializacji
+        public EmployeeInfo()
+        {
+            this.yearOfEmployment = 0;
+            this.skillLevel = 0;
+            this.status = Status.Intern;
+        }
+
+        public override string ToString()
+        {
+            return "year of employment = " + yearOfEmployment + ", skill level = " + skillLevel + ", status = " + status;
+        }
+    }
+}
